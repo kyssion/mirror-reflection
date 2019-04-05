3367c1a45e1f6616e8d74f9ff5c00367f15a79ee
kyssion <1409915687@qq.com>
2019-04-05 21:33:47 +0800

    update

--- src/main/java/org/prims/reflection/agent/Agent.java
+++ src/main/java/org/prims/reflection/agent/Agent.java
@@ -4,10 +4,15 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 
 public interface Agent {
+
     Object invoke(Object target, Object[] args) throws IllegalAccessException, InvocationTargetException;
 
     Class<?> getType();
+
     Class<?>[] getParamType();
+
     Class<?> getReturnType();
+
     Annotation[] getAllAnnotation();
+
 }
