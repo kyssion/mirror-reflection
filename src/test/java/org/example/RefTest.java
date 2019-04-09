2a2e0d4ed8b06f337a60fbc29e9e36c3ca6a7af8
kyssion <1409915687@qq.com>
2019-04-09 19:09:52 +0800

    update

--- src/test/java/org/example/RefTest.java
+++ src/test/java/org/example/RefTest.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RefTest {
-    public static void main(String[] args) throws NoSuchFieldException {
+    public static void main(String[] args) throws NoSuchFieldException, NoSuchMethodException {
         RefItem<String, Number> refItem = new RefItem<>("test", 123,new ArrayList<>());
         Type type = refItem.getClass().getComponentType();
         //返回当前类的内部类的类型如果是匿名内部类,就无效比如new xxx 然后在这个类中使用this.getClass
@@ -25,8 +25,7 @@ public class RefTest {
 
         Type type7 = field.getDeclaringClass();
         Type type8 = field.getType();
-        List<String> list = refItem.getList();
-        Type type6 = list.getClass();
+        Type type6 = field.getGenericType();
         if (type6 instanceof TypeVariable) {
             System.out.println("1");
         } else if (type6 instanceof ParameterizedType) {
@@ -39,10 +38,25 @@ public class RefTest {
 
             System.out.println(4);
         }
+
+        Method method=refItemClass.getMethod("getList",new Class[]{});
+
+       Type type9= method.getGenericReturnType();
+
+        test();
+    }
+    public static void test(){
+        Test test = new Test();
+        Type type = test.getClass().getGenericSuperclass();
+
     }
 }
 
-class Test {
+class Test2<T>{
+
+}
+
+class Test extends Test2<String>{
     private String name;
 
     public String getName() {
@@ -57,18 +71,18 @@ class Test {
 class RefItem<T, S> {
     private T ttt;
     private S s;
-    private List<T> list;
-    RefItem(T ttt, S s,List<T> list) {
+    private List<String> list;
+    RefItem(T ttt, S s,List<String> list) {
         this.ttt = ttt;
         this.s = s;
         this.list = list;
     }
 
-    public List<T> getList() {
+    public List<String> getList() {
         return list;
     }
 
-    public void setList(List<T> list) {
+    public void setList(List<String> list) {
         this.list = list;
     }
 
