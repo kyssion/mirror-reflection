10bc1eb0f0f6b73ba2fc73d772db91ea86cffe21
kyssion <1409915687@qq.com>
2019-05-06 20:42:59 +0800

    添加对包路径扫描的支持

--- src/main/java/org/mirror/reflection/io/DefaultVFS.java
+++ src/main/java/org/mirror/reflection/io/DefaultVFS.java
@@ -15,9 +15,6 @@
  */
 package org.mirror.reflection.io;
 
-import org.apache.ibatis.logging.Log;
-import org.apache.ibatis.logging.LogFactory;
-
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -34,7 +31,6 @@ import java.util.jar.JarInputStream;
  * @author Ben Gunter
  */
 public class DefaultVFS extends VFS {
-  private static final Log log = LogFactory.getLog(DefaultVFS.class);
 
   /** The magic header that indicates a JAR (ZIP) file. */
   private static final byte[] JAR_MAGIC = { 'P', 'K', 3, 4 };
@@ -55,9 +51,6 @@ public class DefaultVFS extends VFS {
       URL jarUrl = findJarForResource(url);
       if (jarUrl != null) {
         is = jarUrl.openStream();
-        if (log.isDebugEnabled()) {
-          log.debug("Listing " + url);
-        }
         resources = listResources(new JarInputStream(is), path);
       }
       else {
@@ -68,13 +61,7 @@ public class DefaultVFS extends VFS {
             // referenced by the URL isn't actually a JAR
             is = url.openStream();
             try (JarInputStream jarInput = new JarInputStream(is)) {
-              if (log.isDebugEnabled()) {
-                log.debug("Listing " + url);
-              }
               for (JarEntry entry; (entry = jarInput.getNextJarEntry()) != null; ) {
-                if (log.isDebugEnabled()) {
-                  log.debug("Jar entry: " + entry.getName());
-                }
                 children.add(entry.getName());
               }
             }
@@ -92,9 +79,6 @@ public class DefaultVFS extends VFS {
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             List<String> lines = new ArrayList<>();
             for (String line; (line = reader.readLine()) != null;) {
-              if (log.isDebugEnabled()) {
-                log.debug("Reader entry: " + line);
-              }
               lines.add(line);
               if (getResources(path + "/" + line).isEmpty()) {
                 lines.clear();
@@ -103,9 +87,6 @@ public class DefaultVFS extends VFS {
             }
 
             if (!lines.isEmpty()) {
-              if (log.isDebugEnabled()) {
-                log.debug("Listing " + url);
-              }
               children.addAll(lines);
             }
           }
@@ -117,13 +98,7 @@ public class DefaultVFS extends VFS {
            */
           if ("file".equals(url.getProtocol())) {
             File file = new File(url.getFile());
-            if (log.isDebugEnabled()) {
-                log.debug("Listing directory " + file.getAbsolutePath());
-            }
             if (file.isDirectory()) {
-              if (log.isDebugEnabled()) {
-                  log.debug("Listing " + url);
-              }
               children = Arrays.asList(file.list());
             }
           }
@@ -190,9 +165,6 @@ public class DefaultVFS extends VFS {
 
         // Check file name
         if (name.indexOf(path) == 0) {
-          if (log.isDebugEnabled()) {
-            log.debug("Found resource: " + name);
-          }
           // Trim leading slash
           resources.add(name.substring(1));
         }
@@ -212,17 +184,11 @@ public class DefaultVFS extends VFS {
    * @throws MalformedURLException
    */
   protected URL findJarForResource(URL url) throws MalformedURLException {
-    if (log.isDebugEnabled()) {
-      log.debug("Find JAR URL: " + url);
-    }
 
     // If the file part of the URL is itself a URL, then that URL probably points to the JAR
     try {
       for (;;) {
         url = new URL(url.getFile());
-        if (log.isDebugEnabled()) {
-          log.debug("Inner URL: " + url);
-        }
       }
     } catch (MalformedURLException e) {
       // This will happen at some point and serves as a break in the loop
@@ -233,14 +199,8 @@ public class DefaultVFS extends VFS {
     int index = jarUrl.lastIndexOf(".jar");
     if (index >= 0) {
       jarUrl.setLength(index + 4);
-      if (log.isDebugEnabled()) {
-        log.debug("Extracted JAR URL: " + jarUrl);
-      }
     }
     else {
-      if (log.isDebugEnabled()) {
-        log.debug("Not a JAR: " + jarUrl);
-      }
       return null;
     }
 
@@ -252,9 +212,6 @@ public class DefaultVFS extends VFS {
       }
       else {
         // WebLogic fix: check if the URL's file exists in the filesystem.
-        if (log.isDebugEnabled()) {
-          log.debug("Not a JAR: " + jarUrl);
-        }
         jarUrl.replace(0, jarUrl.length(), testUrl.getFile());
         File file = new File(jarUrl.toString());
 
@@ -268,9 +225,6 @@ public class DefaultVFS extends VFS {
         }
 
         if (file.exists()) {
-          if (log.isDebugEnabled()) {
-            log.debug("Trying real file: " + file.getAbsolutePath());
-          }
           testUrl = file.toURI().toURL();
           if (isJar(testUrl)) {
             return testUrl;
@@ -278,11 +232,6 @@ public class DefaultVFS extends VFS {
         }
       }
     } catch (MalformedURLException e) {
-      log.warn("Invalid JAR URL: " + jarUrl);
-    }
-
-    if (log.isDebugEnabled()) {
-      log.debug("Not a JAR: " + jarUrl);
     }
     return null;
   }
@@ -320,9 +269,6 @@ public class DefaultVFS extends VFS {
       is = url.openStream();
       is.read(buffer, 0, JAR_MAGIC.length);
       if (Arrays.equals(buffer, JAR_MAGIC)) {
-        if (log.isDebugEnabled()) {
-          log.debug("Found JAR: " + url);
-        }
         return true;
       }
     } catch (Exception e) {
