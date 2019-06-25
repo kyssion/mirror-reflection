0b8518be4c4f07123ecb546976da20b762ce083b
kyssion <1409915687@qq.com>
2019-06-25 19:06:46 +0800

    update

--- src/test/java/org/example/Example1.java
+++ src/test/java/org/example/Example1.java
@@ -1,14 +1,12 @@
 package org.example;
 
 
+import org.mirror.reflection.mirror.MirrorObject;
+
 public class Example1 {
     public static void main(String[] args){
-        double a = 1.0d - 0.9d;
-        double b = 0.9d - 0.8d;
-        if (a == b) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
+
+        MirrorObject mirrorObject = MirrorObject.forObject(new String());
+
     }
 }
