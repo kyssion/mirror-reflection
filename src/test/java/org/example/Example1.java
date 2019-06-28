a8f6ce3d44f3c674ac6e10639578e60320173042
kyssion <1409915687@qq.com>
2019-06-28 17:49:15 +0800

    update

--- src/test/java/org/example/Example1.java
+++ src/test/java/org/example/Example1.java
@@ -1,12 +1,34 @@
 package org.example;
 
 
-import org.mirror.reflection.mirror.MirrorObject;
+import org.mirror.reflection.Reflector;
+import org.mirror.reflection.mirror.MirrorClass;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+@interface Test{
+    public String id();
+}
+
+class Testddd{
+
+}
+@Test(id="123")
+class Testddd2 extends Testddd{
+
+}
 
 public class Example1 {
     public static void main(String[] args){
-
-        MirrorObject mirrorObject = MirrorObject.forObject(new String());
-
+        Testddd testddd2 = new Testddd2();
+        Reflector reflector = new Reflector(testddd2.getClass());
+        MirrorClass mirrorClass = MirrorClass.forClass(testddd2.getClass());
+        Test test = reflector.getAnnotation(Test.class);
+        Test test1 = mirrorClass.getAnnotation(Test.class);
+        System.out.println(test.id());
+        System.out.println(test1.id());
     }
 }
